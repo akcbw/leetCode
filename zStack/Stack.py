deedef386e69190cb4a2fec4ad2e10551a4f2a57
brian.chen <brian.chen755@hotmail.com>
2019-10-28 21:53:12 +0800

    update

--- zStack/Stack.py
+++ zStack/Stack.py
@@ -9,7 +9,7 @@ class MyStack:
         self.stackItems.append(item)
 
     def peek(self):
-        return None if (len(self.stackItems) == 0) else self.stackItems[len(self.stackItems)-1]
+        return None if (len(self.stackItems) == 0) else self.stackItems[-1]
 
     def isEmpty(self):
         return len(self.stackItems) == 0
@@ -19,13 +19,3 @@ class MyStack:
 
     def printStack(self):
         print(self.stackItems)
-
-
-s = MyStack()
-print (s.isEmpty())
-print(s.peek())
-s.push("hha")
-print(s.peek())
-s.push(None)
-print(s.peek())
-s.printStack()
