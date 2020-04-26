d7cd49cf195d9e83316762ea33514ce8169ea46a
brian.chen <brian.chen755@hotmail.com>
2020-04-26 18:48:44 +0800

    update

--- zDP/ClimbStars.py
+++ zDP/ClimbStars.py
@@ -8,6 +8,22 @@ def climbStairs(n):
             dpTable.append(dpTable[i-1] + dpTable[i-2])
     return dpTable[i]
 
+def climbStairsDP(n):
+    dpTable = []
+    for i in range(n):
+        if n < 2:
+            dpTable.append(n + 1)
+        else:
+            dpTable.append(dpTable[n-1] + dpTable[n-2])
+    return dpTable[n]
+
+def climbStairsDP2(n, stairRange):
+    dpTable = [0] * n
+    for i in range(n):
+        for j in stairRange:
+            if i > j:
+                dpTable[n] = dpTable[n] + dpTable[n - j]
+    return dpTable[n]
 
 def climStairsRecursion(n):
     if n < 3:
@@ -25,6 +41,7 @@ def climbStarsRecursionWithMemory(n, memory):
         return climbStarsRecursionWithMemory(n-2, memory) + climbStarsRecursionWithMemory(n-1, memory)
 
 if __name__ == '__main__':
-    print(climbStairs(37))
-    print(climStairsRecursion(37))
-    print(climbStarsRecursionWithMemory(37, {}))
+    # print(climbStairs(37))
+    # print(climStairsRecursion(37))
+    # print(climbStarsRecursionWithMemory(37, {}))
+    print(climbStairsDP2(3, [1,2,3]))
